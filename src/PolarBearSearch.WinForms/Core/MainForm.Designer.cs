ea445a293b259f45abbc548c4e6c79e5b16dcb2b
nikitakaralyus <nikitakaralyus@gmail.com>
2022-01-16 16:28:39 +0300

    Changed designer

--- src/PolarBearSearch.WinForms/Core/MainForm.Designer.cs
+++ src/PolarBearSearch.WinForms/Core/MainForm.Designer.cs
@@ -46,78 +46,47 @@
             this.LeftPanel.Controls.Add(this.SaveButton);
             this.LeftPanel.Controls.Add(this.UploadButton);
             this.LeftPanel.Controls.Add(this.LogoPictureBox);
-            this.LeftPanel.Dock = System.Windows.Forms.DockStyle.Left;
-            this.LeftPanel.Location = new System.Drawing.Point(0, 0);
+            resources.ApplyResources(this.LeftPanel, "LeftPanel");
             this.LeftPanel.Name = "LeftPanel";
-            this.LeftPanel.Size = new System.Drawing.Size(200, 568);
-            this.LeftPanel.TabIndex = 0;
             // 
             // SearchingLabel
             // 
-            this.SearchingLabel.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.SearchingLabel.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.SearchingLabel.Location = new System.Drawing.Point(0, 531);
+            resources.ApplyResources(this.SearchingLabel, "SearchingLabel");
             this.SearchingLabel.Name = "SearchingLabel";
-            this.SearchingLabel.Size = new System.Drawing.Size(200, 37);
-            this.SearchingLabel.TabIndex = 2;
-            this.SearchingLabel.Text = "Searching...";
-            this.SearchingLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.SearchingLabel.Visible = false;
             // 
             // SaveButton
             // 
-            this.SaveButton.Dock = System.Windows.Forms.DockStyle.Top;
-            this.SaveButton.Location = new System.Drawing.Point(0, 250);
+            resources.ApplyResources(this.SaveButton, "SaveButton");
             this.SaveButton.Name = "SaveButton";
-            this.SaveButton.Size = new System.Drawing.Size(200, 50);
-            this.SaveButton.TabIndex = 1;
-            this.SaveButton.Text = "Save";
             this.SaveButton.UseVisualStyleBackColor = true;
             this.SaveButton.Click += new System.EventHandler(this.Save_Click);
             // 
             // UploadButton
             // 
-            this.UploadButton.Dock = System.Windows.Forms.DockStyle.Top;
-            this.UploadButton.Location = new System.Drawing.Point(0, 200);
+            resources.ApplyResources(this.UploadButton, "UploadButton");
             this.UploadButton.Name = "UploadButton";
-            this.UploadButton.Size = new System.Drawing.Size(200, 50);
-            this.UploadButton.TabIndex = 1;
-            this.UploadButton.Text = "Upload";
             this.UploadButton.UseVisualStyleBackColor = true;
             this.UploadButton.Click += new System.EventHandler(this.UploadButton_Click);
             // 
             // LogoPictureBox
             // 
-            this.LogoPictureBox.Dock = System.Windows.Forms.DockStyle.Top;
-            this.LogoPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("LogoPictureBox.Image")));
-            this.LogoPictureBox.Location = new System.Drawing.Point(0, 0);
+            resources.ApplyResources(this.LogoPictureBox, "LogoPictureBox");
             this.LogoPictureBox.Name = "LogoPictureBox";
-            this.LogoPictureBox.Size = new System.Drawing.Size(200, 200);
-            this.LogoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.LogoPictureBox.TabIndex = 0;
             this.LogoPictureBox.TabStop = false;
             // 
             // PictureBox
             // 
-            this.PictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.PictureBox.Location = new System.Drawing.Point(200, 0);
+            resources.ApplyResources(this.PictureBox, "PictureBox");
             this.PictureBox.Name = "PictureBox";
-            this.PictureBox.Size = new System.Drawing.Size(774, 568);
-            this.PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.PictureBox.TabIndex = 1;
             this.PictureBox.TabStop = false;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(974, 568);
             this.Controls.Add(this.PictureBox);
             this.Controls.Add(this.LeftPanel);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Polar Bear Search";
             this.LeftPanel.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.LogoPictureBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
